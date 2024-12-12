dddb69c66a5b21d133bad10cd774ab99b1ddd69b
Olpest22 <olpest2@gmail.com>
2024-12-13 01:58:35 +1000

    1.1

--- src/pages/gifts-page/gifts-page.vue
+++ src/pages/gifts-page/gifts-page.vue
@@ -12,7 +12,7 @@ export default {
 
 <template>
   <div class="wrap">
-    <h1 class="logo">DNS</h1>
+    <img src="/public/image/DNS.svg" class="logo" alt="">
       <div class="column">
         <h2 class="title">Рекомендации <br> подарка</h2>
         <div class="gifts">
@@ -24,11 +24,11 @@ export default {
                 <p class="gift-box__description">Описание товара</p>
               </div>
               <div class="gift-box__flex">
-                <p class="gift-box__price">3000р</p>
+                <p class="gift-box__price">3000₽</p>
                 <a href="/gift"><my-text>Подробнее</my-text></a>
               </div>
             </div>
-          </div>  
+          </div> 
           <div class="gift-box">
             <img class="gift-box__img" src="" alt="">
             <div class="gift-box__margin">
@@ -37,7 +37,7 @@ export default {
                 <p class="gift-box__description">Описание товара</p>
               </div>
               <div class="gift-box__flex">
-                <p class="gift-box__price">3000р</p>
+                <p class="gift-box__price">3000₽</p>
                 <a href="/gift"><my-text>Подробнее</my-text></a>
               </div>
             </div>
@@ -50,7 +50,7 @@ export default {
                 <p class="gift-box__description">Описание товара</p>
               </div>
               <div class="gift-box__flex">
-                <p class="gift-box__price">3000р</p>
+                <p class="gift-box__price">3000₽</p>
                 <a href="/gift"><my-text>Подробнее</my-text></a>
               </div>
             </div>
@@ -63,11 +63,20 @@ export default {
                 <p class="gift-box__description">Описание товара</p>
               </div>
               <div class="gift-box__flex">
-                <p class="gift-box__price">3000р</p>
+                <p class="gift-box__price">3000₽</p>
                 <a href="/gift"><my-text>Подробнее</my-text></a>
               </div>
             </div>
           </div> 
+
+          <img class="star-1" src="/public/image/image 26.svg" alt="">
+          <img class="star-2" src="/public/image/image 30.svg" alt=""> 
+          <img class="star-3" src="/public/image/image 22.svg" alt="">
+          <img class="star-4" src="/public/image/image 22.svg" alt="">
+          <img class="star-5" src="/public/image/image 17.svg" alt="">
+          <img class="star-6" src="/public/image/image 30.svg" alt=""> 
+          <img class="star-7" src="/public/image/image 17.svg" alt="">
+          <img class="star-8" src="/public/image/image 22.svg" alt="">
         </div>
       </div>
   </div>
@@ -90,14 +99,16 @@ export default {
   flex-wrap: wrap;
   max-width: 1369px;
   margin-top: 50px;
-  margin-bottom: 50px;
-  gap: 50px;
+  margin-bottom: 350px;
+  gap: 100px;
+  position: relative;
 }
 
 .logo{
   color: white;
   font-size: 50px;
-  margin-left: 10%;
+  margin-top: 20px;
+  margin-right: 900px;
 }
 
 .title{
@@ -134,13 +145,15 @@ export default {
   flex-direction: column;
   gap: 30px;
   margin: 0 auto;
+  z-index: 1;
+
 }
 
 .gift-box__img{
   height: 341px;
   width: 622px;
   border-radius: 15px;
-  background-color: gray;
+  background-color: #D9D9D9;
 }
 
 .gift-box__flex{
@@ -169,4 +182,44 @@ export default {
   color: #F36A38;
 }
 
+.star-1{
+  position: absolute;
+  top: -200px;
+  left: 200px;
+}
+.star-2{
+  position: absolute;
+  top: -250px;
+  left: 1100px;
+}
+.star-3{
+  position: absolute;
+  top: 420px;
+  left: 300px;
+}
+.star-4{
+  position: absolute;
+  bottom: -210px;
+  left: 300px;
+}
+.star-5{
+  position: absolute;
+  bottom: -300px;
+  right: 200px;
+}
+.star-6{
+  position: absolute;
+  bottom: 650px;
+  right: 490px;
+}
+.star-7{
+  position: absolute;
+  bottom: 300px;
+  right: -100px;
+}
+.star-8{
+  position: absolute;
+  bottom: 0px;
+  right: 480px;
+}
 </style>

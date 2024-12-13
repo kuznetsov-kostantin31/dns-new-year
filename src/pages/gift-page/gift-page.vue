519277465cb90375ea233da76208a7da892f1168
Olpest22 <olpest2@gmail.com>
2024-12-13 14:36:45 +1000

    add stars

--- src/pages/gift-page/gift-page.vue
+++ src/pages/gift-page/gift-page.vue
@@ -13,7 +13,7 @@ export default {
 <template>
  <body>
     <div class="wrap">
-        <h1 class="logo">DNS</h1>
+      <img src="/public/image/DNS.svg" class="logo" alt="">
         <div class="column">
             <h2 class="title">Рекомендации <br> подарка</h2>
             <img class="img" src="" alt="">
@@ -21,6 +21,11 @@ export default {
                 <p class="description">Описание</p>
             </div>
             <my-button>Купить</my-button>
+            <img class="star-1" src="/public/image/image 26.svg" alt="">
+            <img class="star-2" src="/public/image/image 17.svg" alt="">
+            <img class="star-3" src="/public/image/image 22.svg" alt="">
+            <img class="star-4" src="/public/image/image 30.svg" alt="">
+            <img class="star-5" src="/public/image/image 26.svg" alt="">
         </div>
     </div> 
  </body>
@@ -29,22 +34,25 @@ export default {
 
 <style scoped>
 body{
-    background-color: #F36A38;
-    width: 100%;
-    height: 100%;
+  background-color: #F36A38;
+  width: 100%;
+  height: 100%;
 }
-.wrap{
+.wrap {
+  background-color: #F36A38; /* Замените этот цвет на желаемый */
   margin: 0 auto;
   width: 1369px;
-  height: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
 }
 .logo{
   color: white;
   font-size: 50px;
-  margin-left: 10%;
+  width: 140px;
+  margin-left: 170px;
+  margin-top: 20px;
 }
 
 .title{
@@ -57,14 +65,17 @@ body{
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom: 50px;
+  position: relative;
+  margin-bottom: 200px;
+
 }
 
 .img{
   height: 695px;
   width: 1293px;
   border-radius: 15px;
-  background-color: gray;
+  background-color: #D9D9D9;
+  z-index: 1;
 }
 .description{
     font-size: 48px;
@@ -75,5 +86,32 @@ body{
     justify-content: start;
     width: 90%;
     margin-bottom: 50px;
+}
+
+.star-1{
+  position: absolute;
+  left: 100px;
+  transform: rotate(10deg);
+}
+.star-2{
+  position: absolute;
+  right: 100px;
+  bottom: -50px;
+}
+.star-3{
+  position: absolute;
+  left: 100px;
+  bottom: -150px;
+  transform: rotate(15deg);
+}
+.star-4{
+  position: absolute;
+  right: 100px;
+  top: -50px;
+}
+.star-5{
+  position: absolute;
+  left: 570px;
+  bottom: 70px;
 }
 </style>
